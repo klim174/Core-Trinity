3da214c22709aba58d55a6812c68bc5a8290cabf
ahuraa <foruzan_1001@yahoo.com>
2013-08-01 14:40:31 -0500

    DB/GO/Loot: Update: Serpentbloom (5339) (Thx baric)

---
+++ sql/HeroicCore/Update/2013_07_14_04_world_loot_misc.sql
@@ -0,0 +1,4 @@
+-- DB/GO/Loot: Update: Serpentbloom (5339) | by baric
+DELETE FROM `gameobject_loot_template` WHERE `entry`=2772;
+INSERT INTO `gameobject_loot_template` (`entry`,`item`,`ChanceOrQuestChance`,`lootmode`,`groupid`,`mincountOrRef`,`maxcount`)
+VALUES (2772,5339,-100,1,0,1,1);
