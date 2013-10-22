0556a05674c8b775fb21458148bab6a481b3c626
PaulHellCore <pashka2030@bk.ru>
2013-10-23 00:39:17 +0400

    Up

--- src/server/game/Cfbg/Cfbg.cpp
+++ src/server/game/Cfbg/Cfbg.cpp
@@ -54,12 +54,12 @@ void Player::SetFakeRaceAndMorph()
         if (GetOTeam() == ALLIANCE)
         {
             m_FakeMorph = getGender() == GENDER_MALE ? FAKE_M_TAUREN : FAKE_F_TAUREN;
-            m_FakeRace == RACE_TAUREN;
+            m_FakeRace = RACE_TAUREN;
         }
         else if (getGender() == GENDER_MALE) // HORDE PLAYER, ONLY HAVE MALE NELF ID
         {
             m_FakeMorph = FAKE_M_NELF;
-            m_FakeRace == RACE_NIGHTELF;
+            m_FakeRace = RACE_NIGHTELF;
         }
         else
             m_FakeRace = GetOTeam() == ALLIANCE ? RACE_BLOODELF : RACE_HUMAN;
@@ -67,7 +67,7 @@ void Player::SetFakeRaceAndMorph()
     else if (getClass() == CLASS_SHAMAN && GetOTeam() == HORDE && getGender() == GENDER_FEMALE)
     {
         m_FakeMorph = FAKE_F_DRANAEI; // Female Draenei
-        m_FakeRace == RACE_DRAENEI;
+        m_FakeRace = RACE_DRAENEI;
     }
     else
     {
